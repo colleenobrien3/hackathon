209af5ba4653dc0fb0937b3aa73a01bc520493a7
Sara Laffin <sarala@umich.edu>
2020-03-27 10:33:23 -0400

    add models and migrations

--- zoo/models.py
+++ zoo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ZooUser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    newsletter = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Newsletter(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
